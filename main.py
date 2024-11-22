aa531d32692923194da13a6e8b240bd4bdc7efcb
derekshum <derekshum@users.noreply.github.com>
2024-11-21 22:54:53 -0500

    Use geopy to get proper distance calculations

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 import json
+from geopy import distance
 
 
-def parse_coordinates_text(text: str) -> list:
+def parse_to_float(text: str) -> float:
+    """Try to parse text to a float and raise an error if not possible."""
+    try:
+        return float(text)
+    except:
+        raise Exception(f'Unable to parse "{text}" into a numerical value.')
+
+
+def parse_coordinates_text(text: str) -> tuple[float, float]:
     """Returns a tuple of (a, b) from a provided text of 'a, b' or '"a, b"'."""
     text = text.strip('\"').strip(' ')
     text_parts = text.split(',')
@@ -10,13 +19,7 @@ def parse_coordinates_text(text: str) -> list:
         raise Exception('Invalid input: more than one comma')
     elif len(text_parts) < 2:
         raise Exception('Invalid input: no comma')
-    values: list[float] = []
-    for text_part in text_parts:
-        try:
-            values.append(float(text_part))
-        except:
-            raise Exception(f'Unable to parse "{text_part}" into a numerical value')
-    return values
+    return (parse_to_float(text_parts[0]), parse_to_float(text_parts[1]))
 
 
 def get_outdoor_rinks() -> list[dict]:
@@ -30,38 +33,34 @@ def get_outdoor_rinks() -> list[dict]:
     # Datasets are called 'packages'. Each package can contain many 'resources'
     # To retrieve the metadata for this package and its resources, use the package name in this page's URL:
     url = base_url + '/api/3/action/package_show'
-    params = { 'id': 'outdoor-artificial-ice-rinks'}
-    package = requests.get(url, params = params).json()
+    params = {'id': 'outdoor-artificial-ice-rinks'}
+    package = requests.get(url, params=params).json()
 
     # To get resource data:
     for idx, resource in enumerate(package['result']['resources']):
 
-           # for datastore_active resources:
-           if resource['datastore_active']:
-
-               # To selectively pull records and attribute-level metadata:
-               url = base_url + '/api/3/action/datastore_search'
-               p = { 'id': resource['id'] }
-               return requests.get(url, params = p).json()['result']['records']
-               # This API call has many parameters. They're documented here:
-               # https://docs.ckan.org/en/latest/maintaining/datastore.html
-
-
-def get_distance(coordinates_1: list[float], coordinates_2: list[float]):
-    """Returns the distance between two sets of coordinates using pythag."""
-    return ((coordinates_1[0] - coordinates_2[0])**2 + (coordinates_1[1] - coordinates_2[1])**2)**0.5
+        # for datastore_active resources:
+        if resource['datastore_active']:
+            # To selectively pull records and attribute-level metadata:
+            url = base_url + '/api/3/action/datastore_search'
+            p = {'id': resource['id']}
+            return requests.get(url, params=p).json()['result']['records']
+            # This API call has many parameters. They're documented here:
+            # https://docs.ckan.org/en/latest/maintaining/datastore.html
 
 
-def get_rink_values(coordinates: list[float], rink: dict) -> tuple[dict, float]:
+def get_rink_values(coordinates: tuple[float, float], rink: dict) -> tuple[dict, float]:
     """Returns a rink object modified to have its geomtery set as object values, and the distance from the input
     coordinates to the rink."""
     rink["geometry"] = json.loads(rink["geometry"])
-    return (rink, get_distance(coordinates, [rink["geometry"]["coordinates"][1], rink["geometry"]["coordinates"][0]]))
+    print(f'{coordinates}, {(rink["geometry"]["coordinates"][1], rink["geometry"]["coordinates"][0])}')
+    return rink, distance.distance(coordinates,
+                                   (rink["geometry"]["coordinates"][1], rink["geometry"]["coordinates"][0])).km
 
 
-def get_rink_closest_to_coordinates(coordinates: list[float]) -> None:
+def get_rink_closest_to_coordinates(coordinates: tuple[float, float]) -> None:
     """Returns the closest rink to a set of coordinates."""
-    rinks: list[dict] = get_outdoor_rinks() # this gets the latest data, even if locations are not likely to change very often
+    rinks: list[dict] = get_outdoor_rinks()  # get the latest data, even if locations are not likely to change often
     (closest_rink, closest_distance) = get_rink_values(coordinates, rinks[0])
     for i in range(1, len(rinks)):
         rink = rinks[i]
@@ -69,13 +68,17 @@ def get_rink_closest_to_coordinates(coordinates: list[float]) -> None:
         if rink_distance < closest_distance:
             closest_rink = rink
             closest_distance = rink_distance
-    print(f'The closest rink to the input coordinates is {closest_rink["Public Name"]}. It is {closest_distance} away. Its full information is:\n{closest_rink}')
+    print(
+        f'The closest rink to the input coordinates is {closest_rink["Public Name"]}.\nIt is {closest_distance} km '
+        f'away.\nIts full information is: {closest_rink}')
 
 
+def main():
+    print('Enter your coordinates as "latitude, longitude".')
+    coordinates_text = input()
+    coordinates = parse_coordinates_text(coordinates_text)
+    get_rink_closest_to_coordinates(coordinates)
 
 
-print('Enter your coordinates as "latitude, longitude".')
-coordinates_text = input()
-coordinates = parse_coordinates_text(coordinates_text)
-get_rink_closest_to_coordinates(coordinates)
-
+if __name__ == '__main__':
+    main()
